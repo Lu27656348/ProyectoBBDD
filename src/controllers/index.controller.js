7cc91ee04e7e5ef291f764888a95fa1b243f8fa1
Luis Carlos Somoza <luiscarlossomoza@gmail.com>
2021-07-24 18:26:42 -0400

    Tablas principales I
    Se agregaron los crud y las pestañas de las tablas clientes,vehiculos,tipovehiculos, servicios y ciudades

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -40,10 +40,152 @@ const updateServicioByCod = async (req,res) => {
     res.json('LINEA COD ${codlineaaux} UPDATED');
 };
 
+/* MÉTODOS PARA LA TABLA CLIENTE*/
+const getCliente = async (req,res) => {
+    const response = await pool.query('SELECT * FROM CLIENTE');
+    res.json(response.rows);
+};
+/*SELECCIONAR SERVICIO EN ESPECIFICO*/
+const getClienteByCod = async (req,res) => {
+    const codlineaaux = req.params.cedula_cliente;
+    const response = await pool.query('SELECT * FROM CLIENTE WHERE cedula_cliente = $1', [codlineaaux]);
+    res.json(response.rows);
+};
+/*SELECCIONAR SERVICIO EN ESPECIFICO*/
+const postCliente = async (req,res) => {
+    const { cedula_cliente, nombre, email, frecuente, cantidadservicios } = req.body;
+    const response = await pool.query('INSERT INTO CLIENTE (cedula_cliente, nombre, email, frecuente, cantidadservicios) VALUES ($1,$2,$3,$4,$5)',[cedula_cliente, nombre, email, frecuente, cantidadservicios]);
+};
+
+const deleteClienteByCod = async (req,res) => {
+    const codlineaaux = req.params.cedula_cliente;
+    const response = await pool.query('DELETE FROM CLIENTE WHERE cedula_cliente = $1', [codlineaaux]);
+    res.json('CLIENTE COD ${codlineaaux} DELETED');
+};
+
+const updateClienteByCod = async (req,res) => {
+    const codlineaaux = req.params.cedula_cliente;
+    const { cedula_cliente, nombre,email,frecuente,cantidadservicios} = req.body;
+    const response = await pool.query('UPDATE CLIENTE SET cedula_cliente = $1, nombre = $2, email = $3, frecuente = $4, cantidadservicios = $5 WHERE cedula_cliente = $6', [cedula_cliente, nombre,email,frecuente,cantidadservicios,codlineaaux]);
+    res.json('CLIENTE COD ${codlineaaux} UPDATED');
+};
+/* MÉTODOS PARA LA TABLA VEHICULO*/
+const getVehiculo = async (req,res) => {
+    const response = await pool.query('SELECT * FROM VEHICULO');
+    res.json(response.rows);
+};
+/*SELECCIONAR SERVICIO EN ESPECIFICO*/
+const getVehiculoByCod = async (req,res) => {
+    const codlineaaux = req.params.cod_vehiculo;
+    const response = await pool.query('SELECT * FROM VEHICULO WHERE cod_vehiculo = $1', [codlineaaux]);
+    res.json(response.rows);
+};
+/*SELECCIONAR SERVICIO EN ESPECIFICO*/
+const postVehiculo = async (req,res) => {
+    const { cod_vehiculo, modelo, capacidad_tanque, placa, tiempouso, kilometraje, fecha_adquisicion,cod_tipovehiculo,cedula_cliente } = req.body;
+    const response = await pool.query('INSERT INTO VEHICULO (cod_vehiculo, modelo, capacidad_tanque, placa, tiempouso, kilometraje, fecha_adquisicion,cod_tipovehiculo,cedula_cliente) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)',[cod_vehiculo, modelo, capacidad_tanque, placa, tiempouso, kilometraje, fecha_adquisicion,cod_tipovehiculo,cedula_cliente]);
+};
+
+const deleteVehiculoByCod = async (req,res) => {
+    const codlineaaux = req.params.cod_vehiculo;
+    const response = await pool.query('DELETE FROM VEHICULO WHERE cod_vehiculo = $1', [codlineaaux]);
+    res.json('VEHICULO COD ${codlineaaux} DELETED');
+};
+
+const updateVehiculoByCod = async (req,res) => {
+    const codlineaaux = req.params.cod_vehiculo;
+    const { cod_vehiculo, modelo, capacidad_tanque, placa, tiempouso, kilometraje, fecha_adquisicion,cod_tipovehiculo,cedula_cliente} = req.body;
+    const response = await pool.query('UPDATE VEHICULO SET cod_vehiculo = $1, modelo = $2, capacidad_tanque = $3, placa = $4, tiempouso = $5, kilometraje = $6, fecha_adquisicion = $7, cod_tipovehiculo = $8, cedula_cliente = $9 WHERE cod_vehiculo = $10', [ cod_vehiculo, modelo, capacidad_tanque, placa, tiempouso, kilometraje, fecha_adquisicion,cod_tipovehiculo,cedula_cliente,codlineaaux]);
+    res.json('VEHICULO COD ${codlineaaux} UPDATED');
+};
+/* MÉTODOS PARA LA TABLA TIPO-VEHICULO*/
+const getTipoVehiculo = async (req,res) => {
+    const response = await pool.query('SELECT * FROM TIPOVEHICULO');
+    res.json(response.rows);
+};
+/*SELECCIONAR SERVICIO EN ESPECIFICO*/
+const getTipoVehiculoByCod = async (req,res) => {
+    const codlineaaux = req.params.cod_tipovehiculo;
+    const response = await pool.query('SELECT * FROM TIPOVEHICULO WHERE cod_tipovehiculo = $1', [codlineaaux]);
+    res.json(response.rows);
+};
+/*SELECCIONAR SERVICIO EN ESPECIFICO*/
+const postTipoVehiculo = async (req,res) => {
+    const { cod_tipovehiculo, descripcion } = req.body;
+    const response = await pool.query('INSERT INTO TIPOVEHICULO (cod_tipovehiculo, descripcion) VALUES ($1,$2)',[cod_tipovehiculo, descripcion]);
+};
+
+const deleteTipoVehiculoByCod = async (req,res) => {
+    const codlineaaux = req.params.cod_tipovehiculo;
+    const response = await pool.query('DELETE FROM TIPOVEHICULO WHERE cod_tipovehiculo = $1', [codlineaaux]);
+    res.json('TIPOVEHICULO COD ${codlineaaux} DELETED');
+};
+
+const updateTipoVehiculoByCod = async (req,res) => {
+    const codlineaaux = req.params.cod_tipovehiculo;
+    const { cod_tipovehiculo, descripcion} = req.body;
+    const response = await pool.query('UPDATE TIPOVEHICULO SET cod_tipovehiculo = $1, descripcion = $2 WHERE cod_tipovehiculo = $3', [ cod_tipovehiculo,descripcion,codlineaaux]);
+    res.json('VEHICULO COD ${codlineaaux} UPDATED');
+};
+/* MÉTODOS PARA LA TABLA CIUDAD*/
+const getCiudad = async (req,res) => {
+    const response = await pool.query('SELECT * FROM CIUDAD');
+    res.json(response.rows);
+};
+/*SELECCIONAR SERVICIO EN ESPECIFICO*/
+const getCiudadByCod = async (req,res) => {
+    const codlineaaux = req.params.nombre_ciudad;
+    const response = await pool.query('SELECT * FROM CIUDAD WHERE nombre_ciudad = $1', [codlineaaux]);
+    res.json(response.rows);
+};
+/*SELECCIONAR SERVICIO EN ESPECIFICO*/
+const postCiudad = async (req,res) => {
+    const { nombre_ciudad } = req.body;
+    const response = await pool.query('INSERT INTO CIUDAD (nombre_ciudad) VALUES ($1)',[nombre_ciudad]);
+};
+
+const deleteCiudadByCod = async (req,res) => {
+    const codlineaaux = req.params.nombre_ciudad;
+    const response = await pool.query('DELETE FROM CIUDAD WHERE nombre_ciudad = $1', [codlineaaux]);
+    res.json('CIUDAD COD ${codlineaaux} DELETED');
+};
+
+const updateCiudadByCod = async (req,res) => {
+    const codlineaaux = req.params.nombre_ciudad;
+    const { nombre_ciudad } = req.body;
+    const response = await pool.query('UPDATE CIUDAD SET nombre_ciudad = $1 WHERE nombre_ciudad = $2', [ nombre_ciudad, codlineaaux]);
+    res.json('CIUDAD COD ${codlineaaux} UPDATED');
+};
 module.exports = {
+    getCliente,
+    postCliente,
+    getClienteByCod,
+    deleteClienteByCod,
+    updateClienteByCod,
+
+    getTipoVehiculo,
+    postTipoVehiculo,
+    getTipoVehiculoByCod,
+    deleteTipoVehiculoByCod,
+    updateTipoVehiculoByCod,
+
+    getVehiculo,
+    postVehiculo,
+    getVehiculoByCod,
+    deleteVehiculoByCod,
+    updateVehiculoByCod,
+
+    getCiudad,
+    postCiudad,
+    getCiudadByCod,
+    deleteCiudadByCod,
+    updateCiudadByCod,
+
     getServicio,
     postServicio,
     getServicioByCod,
     deleteServicioByCod,
     updateServicioByCod
+
+
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,40 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getServicio, getServicioByCod,postServicio,deleteServicioByCod,updateServicioByCod } = require('../controllers/index.controller');
+const { getServicio, getServicioByCod,postServicio,deleteServicioByCod,updateServicioByCod, 
+        getCliente,getClienteByCod,postCliente,deleteClienteByCod,updateClienteByCod,
+        getVehiculo, getVehiculoByCod,postVehiculo,deleteVehiculoByCod,updateVehiculoByCod,
+        getTipoVehiculo, getTipoVehiculoByCod,postTipoVehiculo,deleteTipoVehiculoByCod,updateTipoVehiculoByCod,
+        getCiudad,getCiudadByCod,postCiudad,deleteCiudadByCod,updateCiudadByCod} = require('../controllers/index.controller');
 
 router.get('/Servicio', getServicio);
 router.get('/Servicio/:cod_servicio', getServicioByCod);
 router.post('/Servicio', postServicio);
 router.delete('/Servicio/:cod_servicio', deleteServicioByCod);
 router.put('/Servicio/:cod_servicio', updateServicioByCod);
+
+router.get('/Vehiculo', getVehiculo);
+router.get('/Vehiculo/:cod_vehiculo', getVehiculoByCod);
+router.post('/Vehiculo', postVehiculo);
+router.delete('/Vehiculo/:cod_vehiculo', deleteVehiculoByCod);
+router.put('/Vehiculo/:cod_vehiculo', updateVehiculoByCod);
+
+router.get('/TipoVehiculo', getTipoVehiculo);
+router.get('/TipoVehiculo/:cod_tipovehiculo', getTipoVehiculoByCod);
+router.post('/TipoVehiculo', postTipoVehiculo);
+router.delete('/TipoVehiculo/:cod_tipovehiculo', deleteTipoVehiculoByCod);
+router.put('/TipoVehiculo/:cod_tipovehiculo', updateTipoVehiculoByCod);
+
+router.get('/Cliente', getCliente);
+router.get('/Cliente/:cedula_cliente', getClienteByCod);
+router.post('/Cliente', postCliente);
+router.delete('/Cliente/:cedula_cliente', deleteClienteByCod);
+router.put('/Cliente/:cedula_cliente', updateClienteByCod);
+
+router.get('/Ciudad', getCiudad);
+router.get('/Ciudad/:nombre_ciudad', getCiudadByCod);
+router.post('/Ciudad', postCiudad);
+router.delete('/Ciudad/nombre_ciudad', deleteCiudadByCod);
+router.put('/Ciudad/:nombre_ciudad', updateCiudadByCod);
 
 module.exports = router;
